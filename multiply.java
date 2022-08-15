c944c092a17d2e70e7fbc6765901be2ac2820aeb
saurabh singh <amreshsingh902681@gmail.com>
2022-08-15 20:31:28 +0530

    multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class sum {
+        public static void main (string[] args){
+            int a=1;
+            int b=2;
+            system.out.println("the sum is" +(a+b));
+        }
+
+}
